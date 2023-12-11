1a463d3f45e72133cfb492aca11bd864fb529801
Vitor Sousa <v.sousa.cf@gmail.com>
2023-12-11 14:11:31 -0300

    fix: changes entities

--- src/entities/patient/dtos/create-patient-dto.ts
+++ src/entities/patient/dtos/create-patient-dto.ts
@@ -5,15 +5,16 @@ import { GenderEnum } from '../../gender';
 export const CreatePatientSchema = z.object({
   email: z.string().email({ message: 'Invalid e-mail format' }),
   name: z.string(),
-  height: z.number().positive(),
-  weight: z.number().positive(),
+  height: z.number().positive().optional(),
+  weight: z.number().positive().optional(),
   birthDate: z
     .string()
     .pipe(z.coerce.date())
     .refine((date) => date <= new Date(), {
       message: 'Birth date cannot be in the future',
-    }),
-  gender: GenderEnum,
+    })
+    .optional(),
+  gender: GenderEnum.optional(),
 });
 
-export type createPatientDTO = z.infer<typeof CreatePatientSchema>;
+export type TCreatePatientDTO = z.infer<typeof CreatePatientSchema>;
