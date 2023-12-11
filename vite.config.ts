98b998bfecb22f37387c8562c1adeb58bf651076
Vitor Sousa <v.sousa.cf@gmail.com>
2023-12-11 14:02:57 -0300

    chore: updated eslint rules, change scripts and update PR template

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@godiet-ui': path.resolve(__dirname, './src/libs/ui/components'),
+      '@godiet-hooks': path.resolve(__dirname, './src/hooks'),
+      '@godiet-entities': path.resolve(__dirname, './src/entities'),
+      '@godiet-components': path.resolve(__dirname, './src/components'),
+    },
+  },
 });
