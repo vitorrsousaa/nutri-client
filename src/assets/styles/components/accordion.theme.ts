53031775ac93e942b0550aa6d42e135747c75fb7
Vitor Sousa <v.sousa.cf@gmail.com>
2023-12-29 22:57:53 -0300

    style: change theme for each component

---
+++ src/assets/styles/components/accordion.theme.ts
@@ -0,0 +1,27 @@
+import { StyleConfig } from '@chakra-ui/theme-tools';
+
+export const accordionTheme: StyleConfig = {
+  baseStyle: {
+    container: {
+      border: 'none',
+      borderBottom: 'solid 1px',
+      borderBottomColor: 'gray.200',
+    },
+    button: {
+      justifyContent: 'space-between',
+      fontSize: '18px',
+      fontWeight: 500,
+      _hover: {
+        color: '#59BD5A',
+        backgroundColor: '#fff',
+      },
+    },
+    icon: {
+      fontSize: '28px',
+    },
+    panel: {
+      fontWeight: 500,
+      color: '#666',
+    },
+  },
+};
---
+++ src/assets/styles/components/table.theme.ts
@@ -0,0 +1,14 @@
+import { StyleConfig } from '@chakra-ui/theme-tools';
+
+export const tableTheme: StyleConfig = {
+  variants: {
+    simple: {
+      th: {
+        borderColor: '#ccc',
+      },
+      td: {
+        borderColor: '#ccc',
+      },
+    },
+  },
+};
