efc33b15d2857f2935e2c297c3f7555d72b2585f
Vitor Sousa <v.sousa.cf@gmail.com>
2024-01-03 17:58:28 -0300

    fix: [NUTR-109] - remove planning meal by patient id service

--- src/hooks/planningMeal.ts
+++ src/hooks/planningMeal.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import PlanningMealService from '../service/Planning';
 
@@ -20,34 +20,5 @@ export function useCreatePlanningMeal(patientId: string) {
   return {
     isCreatingPlanningMeal,
     createPlanningMeal,
-  };
-}
-
-interface useFindPlanningByPatientIdOptions {
-  enabled?: boolean;
-}
-
-export function useFindPlanningByPatientId(
-  id: string | undefined,
-  options: useFindPlanningByPatientIdOptions
-) {
-  const { enabled: enabledOptions } = options;
-
-  const {
-    isLoading: isFetchingPlanningByPatientId,
-    data: planningByPatientId,
-    refetch: refetchPlanningByPatientId,
-  } = useQuery(
-    [`@planningMeal-${id}`, id],
-    () => PlanningMealService.findByPatientId(id),
-    {
-      enabled: enabledOptions,
-    }
-  );
-
-  return {
-    isFetchingPlanningByPatientId,
-    planningByPatientId,
-    refetchPlanningByPatientId,
   };
 }
