541e84bdc813863ed613a5f2a823e3cb87027e5e
Vitor Sousa <v.sousa.cf@gmail.com>
2024-01-07 22:30:33 -0300

    feat: [NUTR-115] - created auto complete component

--- src/libs/ui/components/Select/Select.tsx
+++ src/libs/ui/components/Select/Select.tsx
@@ -6,26 +6,56 @@ import {
 
 import Spinner from '../Spinner';
 
+import { Autocomplete } from './components/Autocomplete';
+
 export type SelectOptionsType = { label: string; value: string };
 
-interface SelectProps extends ChakraSelectProps {
+interface SelectProps extends Omit<ChakraSelectProps, 'autoComplete'> {
   options: SelectOptionsType[];
   isLoading?: boolean;
+  autoComplete?: boolean;
+  autoCompleteOptions?: {
+    noOptionsMessage?: string;
+    placeholder?: string;
+  };
+  value?: SelectOptionsType['value'];
 }
 
 export function Select(props: SelectProps) {
-  const { options, isLoading, ...selectProps } = props;
+  const {
+    options,
+    isLoading,
+    autoComplete,
+    isDisabled,
+    autoCompleteOptions,
+    ...selectProps
+  } = props;
 
   return (
-    <ChakraSelect
-      {...selectProps}
-      icon={isLoading ? <Spinner thickness="2px" /> : <ChevronDownIcon />}
-    >
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </ChakraSelect>
+    <>
+      {autoComplete ? (
+        <Autocomplete
+          isLoading={isLoading}
+          isDisabled={isDisabled}
+          noOptionsMessage={autoCompleteOptions?.noOptionsMessage}
+          placeholder={autoCompleteOptions?.placeholder}
+          options={options}
+          onChange={selectProps.onChange}
+          value={selectProps.value}
+        />
+      ) : (
+        <ChakraSelect
+          {...selectProps}
+          isDisabled={isDisabled || isLoading}
+          icon={isLoading ? <Spinner thickness="2px" /> : <ChevronDownIcon />}
+        >
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </ChakraSelect>
+      )}
+    </>
   );
 }
