e70c387e23db05f03dd61706e9eedbbafec63cd9
Vitor Sousa <v.sousa.cf@gmail.com>
2024-01-09 13:50:23 -0300

    fix: [NUTR-131] - includes prop to enabled request

--- src/hooks/anamneseTemplate.ts
+++ src/hooks/anamneseTemplate.ts
@@ -2,7 +2,12 @@ import AnamnesisTemplateService from '@godiet-services/AnamnesisTemplate';
 
 import { useQuery } from '@tanstack/react-query';
 
-export function useGetAllAnamneseTemplate(id: string) {
+export function useGetAllAnamneseTemplate(
+  id: string = '',
+  options: {
+    enabled?: boolean;
+  }
+) {
   const {
     data: anamneseTemplate,
     isFetching: isFetchingAnamneseTemplate,
@@ -10,6 +15,7 @@ export function useGetAllAnamneseTemplate(id: string) {
   } = useQuery({
     queryKey: [`@anamneseTemplate-${id && id}`, id],
     queryFn: AnamnesisTemplateService.getAll,
+    enabled: options.enabled,
   });
 
   return {
