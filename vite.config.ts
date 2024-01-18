648ee0a9e1b639229a6ba2e39fc94d06e0f60a0a
Vitor Sousa <v.sousa.cf@gmail.com>
2024-01-18 16:58:28 -0300

    chore: increase configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
   resolve: {
     alias: {
       '@godiet-ui': path.resolve(__dirname, './src/libs/ui/components'),
